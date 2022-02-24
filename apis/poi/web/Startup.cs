1403f9d254384f11fd061ef725d5545538c4ea46
Singh Rohit <rohit.singh@volvo.com>
2022-02-24 17:46:23 +0530

    alljobs5

--- apis/poi/web/Startup.cs
+++ apis/poi/web/Startup.cs
@@ -93,7 +93,7 @@ namespace poi
             context.Response.ContentType = "application/json";
 
             var json = new JObject(
-                new JProperty("message", "POI Service Healthcheck blue"),
+                new JProperty("message", "POI Service Healthcheck Red"),
                 new JProperty("status", result.Status.ToString()));
 
             return context.Response.WriteAsync(
